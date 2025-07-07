652f9326eceb175616f4f24bd45822e310fac72a
Men1scus <zzh_cd@126.com>
2025-07-07 19:57:30 +0800

    fix: Fix typo in CUDA environment variable in test scripts

--- bash/test_wllava.sh
+++ bash/test_wllava.sh
@@ -1,4 +1,4 @@
-CUDAS_VISIBLE_DEVICES=0,1 python test/test_wllava.py \
+CUDA_VISIBLE_DEVICES=0,1 python test/test_wllava.py \
 --pretrained_model_name_or_path="preset/models/stable-diffusion-3.5-medium" \
 --transformer_model_name_or_path="preset/models/dit4sr_q" \
 --image_path preset/datasets/test_datasets/ \
