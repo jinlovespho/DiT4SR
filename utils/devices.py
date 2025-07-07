1de9aeb3a49a3f1b00b03bad687d4067b7914fb6
Men1scus <zzh_cd@126.com>
2025-07-07 20:49:17 +0800

    refactor: Remove macOS specific code

--- utils/devices.py
+++ utils/devices.py
@@ -6,15 +6,7 @@ import torch
 #from modules import errors
 
 if sys.platform == "darwin":
-    from modules import mac_specific
-
-
-def has_mps() -> bool:
-    if sys.platform != "darwin":
-        return False
-    else:
-        return mac_specific.has_mps
-
+    raise NotImplementedError("Mac is not yet supported")
 
 def get_cuda_device_string():
     return "cuda"
@@ -23,9 +15,6 @@ def get_cuda_device_string():
 def get_optimal_device_name():
     if torch.cuda.is_available():
         return get_cuda_device_string()
-
-    if has_mps():
-        return "mps"
 
     return "cpu"
 
@@ -44,9 +33,6 @@ def torch_gc():
         with torch.cuda.device(get_cuda_device_string()):
             torch.cuda.empty_cache()
             torch.cuda.ipc_collect()
-
-    if has_mps():
-        mac_specific.torch_mps_gc()
 
 
 def enable_tf32():
